1ed3592bbaa9deaa30a92f5558d1613358bbbb14
megastef <s.thies@bigdata-analyst.de>
2015-03-05 11:47:11 +0100

    simplified config generation in ini file format

--- bin/spmconfig.js
+++ bin/spmconfig.js
@@ -9,22 +9,15 @@
  * This source code is to be used exclusively by users and customers of Sematext.
  * Please see the full license (found in LICENSE in this distribution) for details on its license and the licenses of its dependencies.
  */
-var config = require ('spm-agent').Config
 var fs = require ('fs')
-delete config._
-delete config.rcFlat
-delete config.config
-delete config.agentsToLoad
-
+var spm_token = ""
 if (process.argv.length == 3){
-	config.tokens.spm = process.argv[2]
-	console.log (process.argv[2]) 
+  spm_token = process.argv[2]
 }
-
 if (process.env.SPM_TOKEN)
-		config.tokens.spm = process.env.SPM_TOKEN
+		spm_token = process.env.SPM_TOKEN
 
-
-fs.writeFileSync ('.spmagentrc', JSON.stringify (config, null, '\t'))
-console.log ('Create default config to file: ./.spmagentrc \n' + JSON.stringify  (config, null, '\t'))
+var cfgFileContent = '[tokens]\n  spm = ' + spm_token
+fs.writeFileSync ('.spmagentrc', cfgFileContent)
+console.log ('Create default config to file: ./.spmagentrc \n' + cfgFileContent)
 

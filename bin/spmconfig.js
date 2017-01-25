db18cde8f318a7e5efa3d5d76c2098dea697dfc3
megastef <stefan.thies@sematext.com>
2017-01-25 23:24:44 +0100

    generate default config in YAML format, updated dependencies

--- bin/spmconfig.js
+++ bin/spmconfig.js
@@ -9,8 +9,8 @@
  * Please see the full license (found in LICENSE in this distribution) for details on its license and the licenses of its dependencies.
  */
 var fs = require('fs')
-var os = require ('os')
-var path = require ('path')
+var os = require('os')
+var path = require('path')
 
 var spm_token = ''
 if (process.argv.length === 3) {
@@ -26,21 +26,21 @@ if (os.platform() === 'linux')
 var cfgLines = [
   "# Please don't change this configuration",
   '# Directory for buffered metrics',
-  'useLinuxAgent='+ useLinuxAgent,
-  'dbDir = ' + path.join(__dirname, 'spmdb'),
+  'useLinuxAgent: ' + useLinuxAgent,
+  'dbDir: ' + path.join(__dirname, 'spmdb'),
   ' ',
   '# SPM_MONITOR_TAGS=project:frontend,environment:test,role:testserver',
   '# Application Token for SPM',
-  '[tokens]',
-  '  spm = ' + spm_token,
+  'tokens:',
+  '  spm: ' + spm_token,
   ' ',
-  '[logger]',
+  'logger:',
   '  # log file directory default is __dirname / spmlogs',
-  '  dir = ' + path.join (__dirname,  'spmlogs'),
+  '  dir: ' + path.join(__dirname, 'spmlogs'),
   '  # silent = true means no creation of log files',
-  '  silent = false ',
+  '  silent: false ',
   '  # log level for output - debug, info, error, defaults to error to be quiet',
-  '  level = error '
+  '  level: error '
 ]
 var cfgFileContent = cfgLines.join('\r\n')
 fs.writeFileSync('.spmagentrc', cfgFileContent)

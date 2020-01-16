2099b6a77276952a2bafed9f864b3bff8fd60245
megastef <s.thies@bigdata-analyst.de>
2020-01-16 11:54:30 +0100

    add infra-token to config script, agent startup & readme

--- bin/spmconfig.js
+++ bin/spmconfig.js
@@ -12,11 +12,31 @@ var fs = require('fs')
 var os = require('os')
 var path = require('path')
 
-var spmToken = ''
-if (process.argv.length === 3) {
-  spmToken = process.argv[2]
+var monitoringToken = ''
+var infraToken = ''
+
+if (process.argv.length < 3) {
+  if (!(process.env.SPM_TOKEN || process.env.MONITORING_TOKEN) ||
+      !process.env.INFRA_TOKEN) {
+    console.log('Usage: spmconfig.js YOUR_NODE_APP_TOKEN YOUR_INFRA_TOKEN')
+  }
 }
-if (process.env.SPM_TOKEN) { spmToken = process.env.SPM_TOKEN }
+
+if (process.argv.length > 2) {
+  monitoringToken = process.argv[2]
+}
+
+if (process.argv.length > 3) {
+  infraToken = process.argv[3]
+} else {
+  console.log('Usage: spmconfig.js YOUR_NODE_APP_TOKEN YOUR_INFRA_TOKEN')
+  process.exit(0)
+}
+
+if (process.env.SPM_TOKEN) { monitoringToken = process.env.SPM_TOKEN }
+if (process.env.MONITORING_TOKEN) { monitoringToken = process.env.MONITORING_TOKEN }
+if (process.env.INFRA_TOKEN) { monitoringToken = process.env.SPM_TOKEN }
+
 var useLinuxAgent = 'false'
 if (os.platform() === 'linux') {
   useLinuxAgent = 'true'
@@ -25,16 +45,18 @@ var cfgLines = [
   "# Please don't change this configuration",
   '# Directory for buffered metrics',
   'useLinuxAgent: ' + useLinuxAgent,
-  'dbDir: ' + path.join(__dirname, 'spmdb'),
+  `dbDir: ${path.join(process.cwd(), 'spmdb')}`,
   ' ',
   '# SPM_MONITOR_TAGS=project:frontend,environment:test,role:testserver',
   '# Application Token for SPM',
   'tokens:',
-  '  spm: ' + spmToken,
+  `  spm: ${monitoringToken}`,
+  `  monitoring: ${monitoringToken}`,
+  `  infra: ${infraToken}`,
   ' ',
   'logger:',
   '  # log file directory default is __dirname / spmlogs',
-  '  dir: ' + path.join(__dirname, 'spmlogs'),
+  `  dir: ${path.join(process.cwd(), 'spmlogs')}`,
   '  # silent = true means no creation of log files',
   '  silent: false ',
   '  # log level for output - debug, info, error, defaults to error to be quiet',

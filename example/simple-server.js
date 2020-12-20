acedb80f0c3f0a024e55cf0b17e7c5d6d134af75
Adnan Rahic <adnan.rahic@sematext.com>
2020-12-20 13:36:29 +0100

    remove nodemon and install test dep for spm-agent

--- example/simple-server.js
+++ example/simple-server.js
@@ -1,19 +1,26 @@
 var spmAgent = require('../lib/index.js') // or 'spm-agent-nodejs'
 spmAgent.on('stats', function (stats) {
-  console.log('\nStats\n')
-  console.log(stats)
-  console.log('\n---------------------------------------------------\n')
+  // console.log('\nStats\n')
+  // console.log(stats)
+  // console.log('\n---------------------------------------------------\n')
 })
 spmAgent.on('metric', function (metric) {
-  console.log('\nMetric\n')
-  console.log(metric)
+  // console.log('\nMetric\n')
+  // console.log(process.env.isDocker)
+  // console.log(process.env.containerId)
+  // console.log(process.env.containerName)
+  // console.log(process.env.containerImage)
+  // console.log(metric)
+
   // if (metric.measurement === 'docker') {
   //   console.log(metric)
   // }
-  console.log('\n---------------------------------------------------\n')
+  // console.log('\n---------------------------------------------------\n')
 })
 
 var http = require('http')
-http.createServer(function (req, res) {
-  res.end('Hello World')
-}).listen(3000)
+http
+  .createServer(function (req, res) {
+    res.end('Hello World')
+  })
+  .listen(3000)
